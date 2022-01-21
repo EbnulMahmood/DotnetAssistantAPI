26b769ceddfc5350ae10318fb0bb080cad71201f
EbnulMahmood <shovonmahmood67@gmail.com>
2022-01-21 17:14:09 +0600

    add person model

--- Assistant.Data/Models/Task.cs
+++ Assistant.Data/Models/Task.cs
@@ -4,5 +4,7 @@
     {
         public int Id { get; set; }
         public string? Name { get; set; }
+        public DateTime RequestDate { get; set; }
+        public DateTime DueDate { get; set; }
     }
 }
