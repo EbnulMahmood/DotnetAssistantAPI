4eb3e20d41ac4020cbc108faccceade51db554dd
EbnulMahmood <shovonmahmood67@gmail.com>
2022-02-03 18:35:09 +0600

    add log models

---
+++ AssistantAPI/Models/ExceptionLog.cs
@@ -0,0 +1,8 @@
+﻿namespace AssistantAPI.Models
+{
+    public class ExceptionLog
+    {
+        public int Id { get; set; }
+        public string? Message { get; set; }
+    }
+}
---
+++ AssistantAPI/Models/RequestLog.cs
@@ -0,0 +1,9 @@
+﻿namespace AssistantAPI.Models
+{
+    public class RequestLog
+    {
+        public int Id { get; set; }
+        public string? RequestArrivalTime { get; set; }
+        public string? RequestLeaveTime { get; set; }
+    }
+}
